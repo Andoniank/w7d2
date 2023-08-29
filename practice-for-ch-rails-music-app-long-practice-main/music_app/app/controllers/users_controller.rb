5ae597ebf5c82a472fba8a559805a7bbf636ada3
Andoniank <andoniank@gmail.com>
2023-08-29 13:48:28 -0700

    Add migrations, views and model for User

---
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user.id)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
